d183d2548077beae30124d0aa6a05fcab3d39331
Love Bansal <149326573+bansallove03@users.noreply.github.com>
2025-02-20 22:20:56 +0530

    Time: 0 ms (100.00%) | Memory: 25.2 MB (61.44%) - LeetSync

---
+++ 3324-split-the-array/split-the-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPossibleToSplit(vector<int>& nums) {
+        map<int,int>mpp;
+        for(auto it:nums)mpp[it]++;
+        for(auto it:mpp){
+            if(it.second>2)return false;
+        }
+        return true;
+    }
+};
